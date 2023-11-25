bc17cf8dce427bf0713b5b1d7735dbcd6d7bec73
zakaria darwish <zakaria.safi.darwish@gmail.com>
2023-11-25 11:06:13 +0200

    updating the number of tree

--- random_forest_regression.py
+++ random_forest_regression.py
@@ -6,21 +6,21 @@ from sklearn.tree import DecisionTreeRegressor
 
 # reading the data_set
 dataset = pd.read_csv('/Users/zakariadarwish/Desktop/random_forest_regression/Position_Salaries.csv')
-# feeding the dependant & independant variables
+# feeding the dependant & independent variables
 # where X is dep & y is indep
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 # applying Decision Tree regressor model 
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor = RandomForestRegressor(n_estimators = 500, random_state = 0)
 regressor.fit(X, y)
 
 # predicting a single value using decision tree
 y_pred = regressor.predict([[6.5]])
-
+print(f"y is {y_pred}")
 
 # displaying 
-X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = np.arange(min(X), max(X), 0.001)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
